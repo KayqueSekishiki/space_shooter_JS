f6b3d8c700694a009405ca443161a6b31ed27d9a
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2024-01-28 23:16:24 -0300

    criando inimigos!

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const yourShip = document.querySelector(".player-shooter");
 const playArea = document.querySelector("#main-play-area");
+const aliensImg = [
+  "img/monster-1.png",
+  "img/monster-2.png",
+  "img/monster-3.png",
+];
 
 function flyShip(event) {
   if (event.key === "ArrowUp") {
@@ -55,6 +60,39 @@ function createLaserElement() {
   newLaser.style.left = `${xPosition}px`;
   newLaser.style.top = `${yPosition - 10}px`;
   return newLaser;
+}
+
+function moveLaser(laser) {
+  let laserInterval = setInterval(() => {
+    let xPosition = parseInt(laser.style.left);
+    let aliens = document.querySelectorAll(".alien");
+
+    aliens.forEach((alien) => {
+      if (checkLaserCollision(laser, alien)) {
+        alien.src = "img/explosion.png";
+        alien.classList.remove("alien");
+        alien.classList.add("dead-alien");
+      }
+    });
+
+    if (xPosition === 340) {
+      laser.remove();
+    } else {
+      laser.style.left = `${xPosition + 8}px`;
+    }
+  }, 10);
+}
+
+function createAliens() {
+  let newAlien = document.createElement("img");
+  let alienSprite = aliensImg[Math.floor(Math.random() * aliensImg.length)]; //sorteio de imagens
+  newAlien.src = alienSprite;
+  newAlien.classList.add("alien");
+  newAlien.classList.add("alien-transition");
+  newAlien.style.left = "370px";
+  newAlien.style.top = `${Math.floor(Math.random() * 330) + 30}px`;
+  playArea.appendChild(newAlien);
+  moveAlien(newAlien);
 }
 
 window.addEventListener("keydown", flyShip);
